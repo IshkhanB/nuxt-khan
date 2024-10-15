266017b5c755f7af6922928dd249f78241a2b505
IshkhanB <b2068666@gmail.com>
2024-10-15 21:07:50 +0300

    1

--- server/plugins/extend-html.ts
+++ server/plugins/extend-html.ts
@@ -1,9 +1,9 @@
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('render:html', (html, { event }) => {
     // Это будет объектное представление HTML-шаблона.
-    console.log(html)
+    // console.log(html)
     html.head.push(`<meta name="description" content="Мое индивидуальное описание" />`)
   })
   // Вы также можете перехватить ответ здесь.
-  nitroApp.hooks.hook('render:response', (response, { event }) => { console.log(response) })
+  // nitroApp.hooks.hook('render:response', (response, { event }) => { console.log(response) })
 })
