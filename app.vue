966b620fbbf90ee165c598bf726950d3aca748be
IshkhanB <b2068666@gmail.com>
2024-10-16 15:01:05 +0300

    16.10

--- app.vue
+++ app.vue
@@ -1,4 +1,16 @@
 <template>
+<header>Заголовок страницы</header>
+
+<nav>Меню навигации</nav>
+
+<aside>Боковая колонка SideBar</aside>
+
+<article>
+ Контент - основное содержимое страницы.
+</article>
+
+<footer>Подвал сайта</footer>
+
 <nav>
 <NuxtLink to="http://localhost:3000/about">About </NuxtLink>
 <br>
@@ -12,9 +24,13 @@
   </div>
 </template>
 
+<style>
+
+</style>
+<!-- 
 <style lang="scss">
 @import url("~/assets/css/style.scss");
 h1 {
   font-family: 'FarAwayGalaxy', sans-serif;
 }
-</style>
+</style> -->
